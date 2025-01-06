ad3ab21fb789391e685a835a88b99ac49adafc05
Abhay <aashta2@my.centennialcollege.ca>
2025-01-06 17:34:12 -0500

    test price changed to original price

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ app.use(express.static('public'));
 // app.use(express.json());
 app.use(morgan('combined')); // Log HTTP requests
 
+
 // CORS: Restrict origins to the client URL
 app.use(
   cors({
