a0df9047fee51392b7ac1a56da610e31184c976c
Abhay <aashta2@my.centennialcollege.ca>
2025-04-21 00:56:02 -0400

    fix expressionspage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,7 @@ import Success from './components/Success'
 import AccCourse from './components/courses/AccCourse';
 import UGCLandingPage from './components/Ugc/Ug';
 import VerbsGuidePage from './components/Leadgen/verbsGuide'
-import ExpressionsPage from './components/Leadgen/expressions'
+// import ExpressionsPage from './components/Leadgen/expressions'
 import MistakesPage from './components/Leadgen/mistakes';
 
 
@@ -58,7 +58,7 @@ function App() {
         <Route path="/success" element={<Success/>}/>
         <Route path="/ugc" element={<UGCLandingPage />} />
         <Route path="/mistakes" element={<MistakesPage />} />
-        <Route path="/expressions" element={<ExpressionsPage />} />
+        {/* <Route path="/expressions" element={<ExpressionsPage />} /> */}
         <Route path="/verbsguide" element={<VerbsGuidePage />} />
       </Routes>
     </Router>
