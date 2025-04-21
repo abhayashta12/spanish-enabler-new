9710752c12b5d409e098a328bded01513b78a1fa
Abhay <aashta2@my.centennialcollege.ca>
2025-04-21 00:47:50 -0400

    vercel expression page error resolved

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,6 @@ import Resources from './components/Resources'
 import Success from './components/Success'
 import AccCourse from './components/courses/AccCourse';
 import UGCLandingPage from './components/Ugc/Ug';
-import ExpressionsPage from './components/Leadgen/expressions'
 import VerbsGuidePage from './components/Leadgen/verbsGuide'
 import ExpressionsPage from './components/Leadgen/mistakes'
 import MistakesPage from './components/Leadgen/mistakes';
@@ -57,6 +56,7 @@ function App() {
         <Route path="/courses/Group" element={<GroupCourse />} />
         <Route path="/courses/Acc" element={<AccCourse />} />
         <Route path="/success" element={<Success/>}/>
+        <Route path="/ugc" element={<UGCLandingPage />} />
         <Route path="/mistakes" element={<MistakesPage />} />
         <Route path="/expressions" element={<ExpressionsPage />} />
         <Route path="/verbsguide" element={<VerbsGuidePage />} />
